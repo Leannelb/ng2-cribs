49991062aa2b0f9abb28b2936ebf19be700ad335
Leannelb <leannelaceybyrne@outlook.com>
2019-06-24 20:50:10 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CribListingComponent } from './crib-listing/crib-listing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CribListingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
